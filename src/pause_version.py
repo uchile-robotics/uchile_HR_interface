5c2f460ad2dfadcc0f204348c4a9338fde05b30d
robotica02 <rosangelarispeg@gmail.com>
2024-04-24 13:47:51 -0400

    Cambio de libreria soundplay a sounddevice e implementacion de funciones Say(), Stop()

--- src/pause_version.py
+++ src/pause_version.py
@@ -1,10 +1,15 @@
-#una version muy sencilla para play el speech
-import rospy  # Import ROS for Python
+#librerias de ROS
+import rospy  
 import rospkg
 import numpy as np
 from gtts import gTTS
 from playsound import playsound
-from std_msgs.msg import String, Int32  # Import ROS messages of type String and Int32
+from std_msgs.msg import String, Int32  
+#librerias de sonido
+import sounddevice as sd
+import soundfile as sf
+#for tests 
+import time
 
 
 class Speech(object):
@@ -12,32 +17,29 @@ class Speech(object):
         super(Speech, self).__init__()
         self.args = args
         self.language = 'es'
+        #self._speech_sub = rospy.Subscriber("/speech", String, self.Say)
 
-        # Subscribe to speech messages
-        self._speech_sub = rospy.Subscriber("/speech", String, self.Say)
-
+        #self.paused = False
 
     def Say(self, msg):
+        print("the method was called!")
         """This method will be called everytime a new speech message is received."""
-
-        #habria que decir el canal en el que esta el texto msg.algo
         text = msg.data
-        
-        #Lo que usa la libreria
         speech = gTTS(text = text, tld="com.mx" ,lang = self.language, slow = False)
-
         speech.save("output.wav")
 
-        #como reproducimos el archivo?
-        playsound('output.wav')
-        
+        data, fs = sf.read("output.wav", dtype='float32')  
+        sd.play(data, fs)
+        # time.sleep(10)
+        # self.Stop()
 
+
+    def Stop(self):
+        sd.stop()
 
 def main():
     rospy.init_node('speech_test')  # Create and register the node!
-
-    obj = Speech('args')  # Create an object of type Joy_Template, defined above
-
+    obj = Speech('args')  #instanciate speech
     rospy.spin()  # ROS function that prevents the program from ending - must be used with Subscribers
 
 if __name__ == '__main__':
