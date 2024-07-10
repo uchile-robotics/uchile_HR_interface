f3e98cd753e3e4e6c60ec128deec63cecb60f944
Eduardo1179 <eduardo.silva.g@ug.uchile.cl>
2024-07-10 13:24:48 -0400

    fix erros stetes_go.py

--- src/states_go.py
+++ src/states_go.py
@@ -14,29 +14,6 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'conversation_smach'))
 from conversation_smach.search import NLPProcessor
 
 # Definir el estado que publica en el tópico speech
-class SpeechByInput(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded'], input_keys=['input_data'])
-
-    def execute(self, userdata):
-        rospy.loginfo('Publicando en el tópico /say: %s', userdata.input_data)
-        pub = rospy.Publisher('/say', String, queue_size=10)
-        rospy.sleep(1)  # Esperar un momento para asegurarse de que el nodo está conectado
-        pub.publish(userdata.input_data)
-        return 'succeeded'
-    
-class SpeechByString(smach.State):
-    def __init__(self, speech_text):
-        smach.State.__init__(self, outcomes=['succeeded'], input_keys=['input_data'])
-        self.speech_text = speech_text
-
-    def execute(self, userdata):
-        rospy.loginfo('Publicando en el tópico /say: %s', self.speech_text)
-        pub = rospy.Publisher('/say', String, queue_size=10)
-        rospy.sleep(1)  # Esperar un momento para asegurarse de que el nodo está conectado
-        self.speech_text = userdata.input_data
-        pub.publish(self.speech_text)
-        return 'succeeded'
 
 
 class HearState(smach.State):
@@ -69,7 +46,7 @@ class HearState(smach.State):
 
 class Instruction_command_state(smach.State):
     def __init__(self):
-        smach.State.__init__(self, outcomes=['go_to_ollama'], input_keys=['heared_data'], output_keys=['command'])
+        smach.State.__init__(self, outcomes=['go_to_ollama'], input_keys=['input_data'], output_keys=['command'])
         self.nlp_model = NLPProcessor()
 
     def execute(self, userdata):
@@ -79,31 +56,17 @@ class Instruction_command_state(smach.State):
         return 'go_to_'
 
 
-class ConversationState(smach.State):
-    def __init__(self):
-        smach.State.__init__(self, outcomes=['succeeded'], input_keys=['command'], output_keys=['ollama_answer'])
-        self.nlp_processor = NLPProcessor()
-
-    def execute(self, userdata):
-        rospy.loginfo('Procesando comando con Ollama: %s', userdata.command)
-        ollama_answer = self.nlp_processor.process_query(userdata.command)
-        rospy.loginfo('Respuesta de Ollama: %s', ollama_answer)
-        userdata.ollama_answer = ollama_answer
-        return 'succeeded'
-
 
 def main():
     rospy.init_node('smach_speech_node')
 
     # Crear una máquina de estado SMACH
-    sm = smach.StateMachine(outcomes=['succeeded', 'timeout'])
+    sm = smach.StateMachine(outcomes=["go_to_"])
 
     # Añadir estados a la máquina de estado
-
-
     with sm:
         smach.StateMachine.add('HEAR', HearState(), transitions={'heared':'Commands', 'timeout':'HEAR'}, remapping={'heared_data':'heared_data'})
-        smach.StateMachine.add('Commands', Instruction_command_state(), transitions={'go_to_':'go_to_'}, remapping={'heared_data':'heared_data', 'command':'command'})
+        smach.StateMachine.add('Commands', Instruction_command_state(), transitions={'go_to_':'go_to_'}, remapping={'input_data':'heared_data', 'command':'command'})
         #smach.StateMachine.add('SPEECH', SpeechByInput(), transitions={'succeeded':'succeeded'}, remapping={'input_data':'ollama_answer'})
 
     # Ejecutar la máquina de estado
